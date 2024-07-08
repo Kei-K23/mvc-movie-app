a7e1459cbaaf0e38ee155376a669f0666b1ff6c7
Kei-K23 <keiksl2301@gmail.com>
2024-07-08 15:51:19 +0630

    Add README.md

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -6,13 +6,24 @@ namespace MvcMovie.Models
     public class Movie
     {
         public Guid Id { get; set; }
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
         public required string Title { get; set; }
         [Display(Name = "Relase Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+        [Required]
+        [StringLength(30)]
         public required string Genre { get; set; }
+        [Range(1, 100)]
         [Column(TypeName = "decimal(18, 2)")]
+        [Required]
         public decimal Price { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+        [StringLength(5)]
+        [Required]
         public string? Rating { get; set; }
 
     }
---
+++ README.md
@@ -0,0 +1,3 @@
+# Movie CRUD .NET MVC website
+
+This is simple website that build with C# + .NET Core MVC that perform CRUD operations.
